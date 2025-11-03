14a7d41f08e7492c7aebc6ad64f2bb289811ee91
jensadev <jensandreasson77@gmail.com>
2025-11-03 10:05:25 +0100

    add main game logic and README file

---
+++ main.js
@@ -0,0 +1,29 @@
+// const playerName = prompt("Skriv ditt namn: ")
+const playerName = "Jens" // statiskt så vi slipper prompt varje körning
+let playerHp = 100
+let enemyHp = 100
+
+function rollDice() {
+    return Math.ceil(Math.random() * 6)
+}
+
+console.log(rollDice())
+
+let playerRoll = rollDice()
+let enemyRoll = rollDice()
+
+function gameRound() {
+    if (playerRoll > enemyRoll) {
+        const damage = playerRoll - enemyRoll
+        console.log(`Du köttar fienden för ${damage}!`)
+        enemyHp -= damage
+    } else if (enemyRoll > playerRoll) {
+        const damage = enemyRoll - playerRoll
+        console.log(`Nedrans, du blir mulad för ${damage}!`)
+        playerHp -= damage
+    } else {
+        console.log("Snyggt parerat, inget händer!")
+    }
+}
+
+gameRound()

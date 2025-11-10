84daa2e748bf30ef48d3d05ab316c28da42cb398
LJoki06 <leo.jokijarvi@elev.ga.ntig.se>
2025-11-10 09:38:02 +0100

    coola saker

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const playerName = "Jesus"
-let playerHp = 10
-let enemyHp = 10
+let playerHp = 50
+let enemyHp = 50
 
 function rollDice() {
     return Math.ceil(Math.random() * 6)
@@ -9,13 +9,24 @@ function rollDice() {
 const playerHpElement = document.querySelector("#player-hp")
 const enemyHpElement = document.querySelector("#enemy-hp")
 
+const combatLogElement = document.querySelector("#combat-log")
+
+function log(msg) {
+    const li = document.createElement("li")
+    li.textContent = msg
+    combatLogElement.appendChild(li)
+    if (combatLogElement.childNodes.length > 10) {
+        combatLogElement.removeChild(combatLogElement.firstChild)
+    }
+}
+
 function gameEnd() {
     if (enemyHp <=  0){
-        console.log("Spelaren har segrat över fienden!")
+        log("Spelaren har segrat över fienden!")
         playButton.removeEventListener("click", gameRound)
     }
     else if (playerHp <= 0) {
-        console.log("Fienden har segrat över spelaren!")
+        log("Fienden har segrat över spelaren!")
         playButton.removeEventListener("click", gameRound)
     }
 }
@@ -25,14 +36,14 @@ function gameRound() {
     const eRoll = rollDice()
     if (pRoll > eRoll) {
         const damage = pRoll - eRoll
-        console.log(`Du vinner med en ${pRoll}a mot fiendens ${eRoll}a. Fienden tar ${damage} skada!`)
+        log(`Du vinner med en ${pRoll}a mot fiendens ${eRoll}a. Fienden tar ${damage} skada!`)
         playerHp -= damage
     } else if (eRoll > pRoll) {
         const damage = eRoll - pRoll
-        console.log(`Fienden vinner med en ${eRoll}a mot din ${pRoll}a. Du tar ${damage} skada!`)
+        log(`Fienden vinner med en ${eRoll}a mot din ${pRoll}a. Du tar ${damage} skada!`)
         enemyHp -= damage
     } else {
-        console.log("Snyggt parerat, inget händer!")
+        log("Snyggt parerat, inget händer!")
     }
     playerHpElement.textContent = playerHp
     enemyHpElement.textContent = enemyHp

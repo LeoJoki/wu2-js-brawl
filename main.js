1668276a69bf106898deb6762325e2d569378d10
LJoki06 <leo.jokijarvi@elev.ga.ntig.se>
2025-11-10 08:36:34 +0100

    l

--- main.js
+++ main.js
@@ -1,28 +1,45 @@
 const playerName = "Jesus"
-let playerHp = 100
-let enemyHp = 100
+let playerHp = 10
+let enemyHp = 10
 
 function rollDice() {
     return Math.ceil(Math.random() * 6)
 }
 
-console.log(rollDice())
+const playerHpElement = document.querySelector("#player-hp")
+const enemyHpElement = document.querySelector("#enemy-hp")
 
-let pRoll = rollDice
-let eRoll = rollDice
-
-function gameRound() {
-    if (pRoll > eRoll) {
-        const damage = pRoll - eRoll
-        console.log("Du vinner med en ${pRoll}a mot fiendens ${eRoll}a. Fienden tar ${damage} skada!")
-        enemyHp - damage
-    } else if (eRoll > pRoll) {
-        const damage = eRoll - pRoll
-        console.log("Fienden vinner med en " (eRoll), "a mot din" (pRoll), "a. Du tar" (damage), "skada!")
-        enemyHp - damage
-    } else {
-        console.log("Snyggt parerat, inget händer!")
+function gameEnd() {
+    if (enemyHp <=  0){
+        console.log("Spelaren har segrat över fienden!")
+        playButton.removeEventListener("click", gameRound)
+    }
+    else if (playerHp <= 0) {
+        console.log("Fienden har segrat över spelaren!")
+        playButton.removeEventListener("click", gameRound)
     }
 }
 
-gameRound()
+function gameRound() {
+    const pRoll = rollDice()
+    const eRoll = rollDice()
+    if (pRoll > eRoll) {
+        const damage = pRoll - eRoll
+        console.log(`Du vinner med en ${pRoll}a mot fiendens ${eRoll}a. Fienden tar ${damage} skada!`)
+        playerHp -= damage
+    } else if (eRoll > pRoll) {
+        const damage = eRoll - pRoll
+        console.log(`Fienden vinner med en ${eRoll}a mot din ${pRoll}a. Du tar ${damage} skada!`)
+        enemyHp -= damage
+    } else {
+        console.log("Snyggt parerat, inget händer!")
+    }
+    playerHpElement.textContent = playerHp
+    enemyHpElement.textContent = enemyHp
+    gameEnd()
+}
+
+
+
+const playButton = document.querySelector("#play-button")
+playButton.addEventListener("click", gameRound)
